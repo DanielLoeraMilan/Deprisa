3665d032761e842cfac5411c89c9fe7e163a52e5
DanielLoeraMilan <daniel1000an@gmail.com>
2022-05-18 23:35:11 -0500

    Add Items

--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    VagonetaHud hud = new VagonetaHud();
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -18,14 +18,22 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         prepare();
+        addObject(hud,0,0);
     }
+    
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
      */
     private void prepare()
     {
-        Vagoneta vagoneta = new Vagoneta();
-        addObject(vagoneta,124,192);
+        //Vagoneta vagoneta = new Vagoneta();
+        addObject(new Vagoneta(hud),124,192);
+        Coin coin = new Coin();
+        addObject(coin,505,54);
+        Coin coin2 = new Coin();
+        addObject(coin2,76,346);
+        Tuba tuba = new Tuba();
+        addObject(tuba,505,328);
     }
 }
--- Vagoneta.java
+++ Vagoneta.java
@@ -8,13 +8,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Vagoneta extends Actor
 {
+    private int offsetX=0;
+    private int offsetY=0;
+    private int score;
+    private VagonetaHud vagonetaHud;
+    
     /**
      * Act - do whatever the Vagoneta wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Vagoneta(VagonetaHud vagonetaHud){
+        this.vagonetaHud = vagonetaHud;
+    }
+    
     public void act()
     {
         moveVagoneta();
+        
+        checkColissions();
     }
     
     private void moveVagoneta(){
@@ -39,6 +50,22 @@ public class Vagoneta extends Actor
         
             if(Greenfoot.isKeyDown("right")){
                 turn(-2);
+            }
+        }
+    }
+    
+    private void checkColissions(){
+        Item item = (Item)getOneIntersectingObject(Item.class);
+        
+        if(item != null){
+            getWorld().removeObject(item);
+            score += item.getScore();
+            vagonetaHud.setScore(score);
+            
+            if(getWorld().getObjects(Item.class).isEmpty()){
+                offsetX=0;
+                offsetY=0;
+                getWorld().showText("GAME OVER",350,250);
             }
         }
     }
